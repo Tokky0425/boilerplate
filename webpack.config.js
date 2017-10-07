b32768398e68bd2958304009b83e9220b633b299
Tokky0425 <lily.yarn.5@gmail.com>
2017-10-08 00:05:16 +0900

    add files

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = [
+  {
+    entry: './src/js/app.js',
+    output: {
+      path: path.join(__dirname, 'public/assets'),
+      filename: 'bundle.js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }
+      ]
+    },
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+    }
+  },
+  {
+    entry: './src/sass/style.scss',
+    output: {
+      path: path.join(__dirname, 'public/assets'),
+      filename: 'style.css'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.scss$/,
+          loader: ExtractTextPlugin.extract(
+            'css-loader!sass-loader'
+          )
+        }
+      ]
+    },
+    plugins: [
+      new ExtractTextPlugin('style.css')
+    ]
+  }
+];
